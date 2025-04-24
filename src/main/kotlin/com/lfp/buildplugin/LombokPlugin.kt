830d332efb9d5f4ae30f87ada8ef40677373b9c4
regbo <reggie.pierce@gmail.com>
2025-04-24 03:10:30 -0400

    Update LombokPlugin.kt

--- src/main/kotlin/com/lfp/buildplugin/LombokPlugin.kt
+++ src/main/kotlin/com/lfp/buildplugin/LombokPlugin.kt
@@ -1,16 +1,28 @@
 package com.lfp.buildplugin
 
+import org.gradle.api.Action
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
 class LombokPlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.extensions.extraProperties["lombok"] = Utils.closure {
-            project.logger.lifecycle("adding lombok")
-            val dependencyNotation = VersionCatalog.instance.findLibrary("lombok")!!.dependencyNotation(project)
-            project.dependencies.add("annotationProcessor", dependencyNotation)
-            project.dependencies.add("compileOnly", dependencyNotation)
-            project.dependencies.add("testAnnotationProcessor", dependencyNotation)
+            project.afterEvaluate(object : Action<Project> {
+                override fun execute(project: Project) {
+                    configureProjectAfterEvaluate(project)
+                }
+            })
+        }
+    }
+
+    private fun configureProjectAfterEvaluate(project: Project) {
+        project.logger.lifecycle("adding lombok")
+        val dependencyNotation = VersionCatalog.instance.findLibrary("lombok")!!.dependencyNotation(project)
+        listOf("annotationProcessor", "compileOnly", "testAnnotationProcessor").forEach { configurationName ->
+            val configuration = project.configurations.findByName(configurationName)
+            if (configuration != null) {
+                project.dependencies.add(configuration.name, dependencyNotation)
+            }
         }
     }
 
