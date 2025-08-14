ace6006ceab9b8dd10108a6c0d4785c02d371a98
regbo <reggie.pierce@gmail.com>
2025-08-14 01:03:43 -0400

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,3 @@
-
-pluginManagement {
-
-buildscript{
-    dependencies {
-
-    }
-}
-
-
 // === Set the root project name from a Gradle property ===
 rootProject.name = providers.gradleProperty("repository_name").get()
 
